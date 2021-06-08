a3c37f5e42a1a49dea4cd0bb530308e766dc71fe
Derek Louis <thedereklouis@gmail.com>
2021-06-08 02:04:45 -0400

    have working game with high score

--- src/Elevator.js
+++ src/Elevator.js
@@ -1,23 +1,50 @@
 import './Elevator.css';
-import { useRef, useState } from 'react';
+import { useRef, useState, useEffect } from 'react';
 
 function App() {
+  useEffect(() => {
+    if (localStorage.getItem('elevatorHighScore') === null) {
+      localStorage.setItem('elevatorHighScore', 0);
+    }
+  }, []);
   const buttons = useRef([]);
+
+  const [gameState, setGameState] = useState(false);
+
   const [scoreCount, setScoreCount] = useState(0);
+  const scoreCountRef = useRef(0);
+  const [highScore, setHighScore] = useState(
+    localStorage.getItem('elevatorHighScore') || 0
+  );
+
   const buttonsArr = [];
+
   const buttonClick = (e) => {
     const buttonId = Number(e.target.innerText - 1);
-
-    if (!buttons.current[buttonId].classList.contains('on')) {
-      buttons.current[buttonId].classList.add('on');
+    if (gameState) {
+      if (!buttons.current[buttonId].classList.contains('on')) {
+        if (scoreCount > 0) {
+          setScoreCount(scoreCount - 1);
+          scoreCountRef.current--;
+        }
+      } else {
+        buttons.current[buttonId].classList.remove('on');
+        if (scoreCount < 100) {
+          setScoreCount(scoreCount + 1);
+          scoreCountRef.current++;
+        }
+        const random = Math.floor(Math.random() * 42);
+        buttons.current[random].classList.add('on');
+      }
     } else {
-      buttons.current[buttonId].classList.remove('on');
-    }
-    if (scoreCount < 100) {
-      setScoreCount(scoreCount + 1);
+      if (!buttons.current[buttonId].classList.contains('on')) {
+        buttons.current[buttonId].classList.add('on');
+      } else {
+        buttons.current[buttonId].classList.remove('on');
+      }
     }
   };
-  for (let i = 0; i < 24; i++) {
+  for (let i = 0; i < 42; i++) {
     buttonsArr.push(
       <button
         key={i}
@@ -29,6 +56,37 @@ function App() {
       </button>
     );
   }
+  const divClick = (e) => {
+    if (gameState && e.target.id === 'buttonsDiv') {
+      if (scoreCount > 0) {
+        setScoreCount(scoreCount - 1);
+        scoreCountRef.current--;
+      }
+    }
+  };
+  const startGame = () => {
+    for (let i = 0; i < 42; i++) {
+      buttons.current[i].classList.remove('on');
+    }
+    setGameState(true);
+    setScoreCount(0);
+    scoreCountRef.current = 0;
+    const random = Math.floor(Math.random() * 42);
+    buttons.current[random].classList.add('on');
+    setTimeout(() => {
+      setGameState(false);
+      for (let i = 0; i < 42; i++) {
+        buttons.current[i].classList.remove('on');
+      }
+      if (
+        scoreCountRef.current >
+        Number(localStorage.getItem('elevatorHighScore'))
+      ) {
+        localStorage.setItem('elevatorHighScore', scoreCountRef.current);
+        setHighScore(scoreCountRef.current);
+      }
+    }, 30000);
+  };
   return (
     <div id="ElevatorContainer">
       <h1 id="title">ELEVATOR</h1>
@@ -37,10 +95,10 @@ function App() {
         <div
           id="needle"
           style={{ transform: `rotate(${-50 + scoreCount}deg)` }}
-        ></div>
-        <div id="puck"></div>
+        />
+        <div id="puck" />
       </div>
-      <div id="buttonsDiv">
+      <div id="buttonsDiv" onClick={divClick}>
         {buttonsArr}
         <div className="screw screwTopLeft">
           <div className="x xTopLeft">x</div>
@@ -55,6 +113,10 @@ function App() {
           <div className="x xBottomRight">x</div>
         </div>
       </div>
+      <button id="goButton" onClick={startGame} disabled={gameState}>
+        GO!
+      </button>
+      <p id="highScore">HIGH SCORE: {highScore}</p>
     </div>
   );
 }
